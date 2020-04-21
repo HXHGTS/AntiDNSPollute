fded160ee307900544a343aae2c8249f92900877
HXHGTS <38959826+StarLYSS@users.noreply.github.com>
2020-04-21 23:05:31 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 ﻿#include<stdio.h>
 #include<stdlib.h>
 
-int mode;
+int mode,DNSSet;
 FILE* yaml;
 int main() {
 	MainMenu:system("cls");
@@ -71,14 +71,59 @@ int main() {
 		goto MainMenu;
 	}
 	else if (mode == 1) {
+		printf("使用仅支持ipv4的DNS时请关闭计算机的ipv6功能，避免优先使用ipv6 DNS解析！\n\n");
+		printf("请选择你想用的DNS：\n\n1.韩国KT DNS（仅ipv4）\n\n2.韩国SK DNS（仅ipv4）\n\n3.韩国LG U+ DNS（仅ipv4）\n\n4.台湾中华电信HiNet DNS（仅ipv4）\n\n5.台湾TWNIC DNS（ipv4+ipv6）\n\n6.中国大陆CNNIC DNS（ipv4+ipv6）\n\n请输入：");
+		scanf("%d", &DNSSet);
+		system("cls");
 		printf("可能会出现较长时间黑屏，请耐心等待. . .\n");
-		system("netsh interface ip set dns \"以太网\" static 117.50.10.10");
-		system("netsh interface ip add dns \"以太网\" 52.80.52.52 index=2");
-		system("netsh interface ip set dns \"WLAN\" static 117.50.10.10");
-		system("netsh interface ip add dns \"WLAN\" 52.80.52.52 index=2");
-		system("netsh interface ipv6 set dns \"以太网\" static ::1");
-		system("netsh interface ipv6 set dns \"WLAN\" static ::1");
-		printf("DNS解析服务器已成功设置为ONE DNS！\n\n");
+		if (DNSSet == 1) {
+			system("netsh interface ip set dns \"以太网\" static 168.126.63.1");
+			system("netsh interface ip add dns \"以太网\" 168.126.63.2 index=2");
+			system("netsh interface ip set dns \"WLAN\" static 168.126.63.1");
+			system("netsh interface ip add dns \"WLAN\" 168.126.63.2 index=2");
+		}
+		else if (DNSSet == 2) {
+			system("netsh interface ip set dns \"以太网\" static 210.220.163.82");
+			system("netsh interface ip set dns \"WLAN\" static 210.220.163.82");
+		}
+		else if (DNSSet == 3) {
+			system("netsh interface ip set dns \"以太网\" static 164.124.101.2");
+			system("netsh interface ip add dns \"以太网\" 203.248.252.2 index=2");
+			system("netsh interface ip set dns \"WLAN\" static 164.124.101.2");
+			system("netsh interface ip add dns \"WLAN\" 203.248.252.2 index=2");
+		}
+		else if (DNSSet == 4) {
+			system("netsh interface ip set dns \"以太网\" static 168.95.1.1");
+			system("netsh interface ip add dns \"以太网\" 168.95.192.1 index=2");
+			system("netsh interface ip set dns \"WLAN\" static 168.95.1.1");
+			system("netsh interface ip add dns \"WLAN\" 168.95.192.1 index=2");
+		}
+		else if (DNSSet == 5) {
+			system("netsh interface ip set dns \"以太网\" static 101.101.101.101");
+			system("netsh interface ip add dns \"以太网\" 101.102.103.104 index=2");
+			system("netsh interface ip set dns \"WLAN\" static 101.101.101.101");
+			system("netsh interface ip add dns \"WLAN\" 101.102.103.104 index=2");
+			system("netsh interface ipv6 set dns \"以太网\" static 2001:de4::101");
+			system("netsh interface ipv6 set dns \"以太网\" static 2001:de4::102");
+			system("netsh interface ipv6 set dns \"WLAN\" static 2001:de4::101");
+			system("netsh interface ipv6 set dns \"WLAN\" static 2001:de4::102");
+		}
+		else {
+			system("netsh interface ip set dns \"以太网\" static 1.2.4.8");
+			system("netsh interface ip add dns \"以太网\" 210.2.4.8 index=2");
+			system("netsh interface ip set dns \"WLAN\" static 1.2.4.8");
+			system("netsh interface ip add dns \"WLAN\" 210.2.4.8 index=2");
+			system("netsh interface ipv6 set dns \"以太网\" static 240c::6666");
+			system("netsh interface ipv6 set dns \"以太网\" static 240c::6644");
+			system("netsh interface ipv6 set dns \"WLAN\" static 240c::6666");
+			system("netsh interface ipv6 set dns \"WLAN\" static 240c::6644");
+		}
+		if(DNSSet==1)printf("DNS解析服务器已成功设置为%s！\n\n","韩国KT DNS");
+		else if (DNSSet == 2)printf("DNS解析服务器已成功设置为%s！\n\n", "韩国SK DNS");
+		else if (DNSSet == 3)printf("DNS解析服务器已成功设置为%s！\n\n", "韩国LG U+ DNS");
+		else if (DNSSet == 4)printf("DNS解析服务器已成功设置为%s！\n\n", "台湾中华电信HiNet DNS");
+		else if (DNSSet == 5)printf("DNS解析服务器已成功设置为%s！\n\n", "台湾TWNIC DNS");
+		else printf("DNS解析服务器已成功设置为%s！\n\n", "中国大陆CNNIC DNS");
 		printf("此模式下，谷歌、油管、脸书、推特、Twitch等敏感域名仍会被污染！\n\n");
 		printf("需要完全解析敏感域名请使用模式2！\n\n");
 		system("ipconfig /flushdns");
@@ -121,7 +166,7 @@ int main() {
 
 int Config_gen() {
 	int area;
-	printf("指定上游DNS服务器区域：\n1.红鱼DNS(亚洲东部)\n2.红鱼DNS(美国西部)\n3.GoogleDNS\n4.OpenDNS\n5.AdGuardDNS\n6.CloudflareDNS\n请输入：");
+	printf("指定上游DNS服务器区域：\n\n1.红鱼DNS（亚洲东部）\n\n2.红鱼DNS（美国西部）\n\n3.GoogleDNS（中国香港）\n\n4.OpenDNS（中国香港）\n\n5.AdGuardDNS（日本东京）\n\n6.CloudflareDNS（日本大阪）\n\n请输入：");
 	scanf("%d", &area);
 		yaml = fopen("AdGuardHome.yaml", "w");
 		if(area==1)fprintf(yaml, "bind_host: 0.0.0.0\nbind_port: 80\nusers:\n- name: root\n  password: $2a$10$mdmUKhmmhuDQO8GHlU.8xOWzwG6Fy.5o5YUCUWYg2DkyfgYRsM0hy\nhttp_proxy: ""\nlanguage: ""\nrlimit_nofile: 0\nweb_session_ttl: 720\ndns:\n  bind_host: 0.0.0.0\n  port: 53\n  statistics_interval: 1\n  querylog_enabled: false\n  querylog_interval: 90\n  querylog_size_memory: 1000\n  anonymize_client_ip: false\n  protection_enabled: true\n  blocking_mode: default\n  blocking_ipv4: \"\"\n  blocking_ipv6: \"\"\n  blocked_response_ttl: 10\n  ratelimit: 0\n  ratelimit_whitelist: []\n  refuse_any: true\n  bootstrap_dns:\n  - 117.50.10.10\n  all_servers: true\n  edns_client_subnet: true\n  enable_dnssec: false\n  aaaa_disabled: false\n  allowed_clients: []\n  disallowed_clients: []\n  blocked_hosts: []\n  parental_block_host: family-block.dns.adguard.com\n  safebrowsing_block_host: standard-block.dns.adguard.com\n  cache_size: 0\n  cache_ttl_min: 0\n  cache_ttl_max: 0\n  upstream_dns:\n%s  - '[/netease.com/]117.50.10.10'\n  - '[/126.net/]117.50.10.10'\n  - '[/icourse163.org/]117.50.10.10'\n  - '[/yeah.net/]117.50.10.10'\n  - '[/126.com/]117.50.10.10'\n  - '[/163.com/]117.50.10.10'\n  - '[/zhangmuhan.club/]117.50.10.10'\n  - '[/google.cn/]117.50.10.10'\n  - '[/baidu.com/]117.50.10.10'\n  - '[/baidupcs.com/]117.50.10.10'\n  - '[/bdstatic.com/]117.50.10.10'\n  - '[/bilibili.com/]117.50.10.10'\n  - '[/bilivideo.com/]117.50.10.10'\n  - '[/hdslb.com/]117.50.10.10'\n  - '[/qq.com/]117.50.10.10'\n  - '[/douyu.com/]117.50.10.10'\n  - '[/huya.com/]117.50.10.10'\n  - '[/aliyun.com/]117.50.10.10'\n  - '[/alicdn.com/]117.50.10.10'\n  - '[/alipay.com/]117.50.10.10'\n  - '[/taobao.com/]117.50.10.10'\n  - '[/tmall.com/]117.50.10.10'\n  - '[/jd.com/]117.50.10.10'\n  - '[/zxxk.com/]117.50.10.10'\n  - '[/cnzz.com/]117.50.10.10'\n  - '[/chaoxing.com/]117.50.10.10'\n  - '[/snnu.edu.cn/]117.50.10.10'\n  - '[/ipip.net/]117.50.10.10'\n  - '[/7moor.com/]117.50.10.10'\n  - '[/t.cn/]117.50.10.10'\n  - '[/chinaz.com/]117.50.10.10'\n  - '[/uzer.me/]117.50.10.10'\n  - '[/lanzou.com/]117.50.10.10'\n  - '[/lanzous.com/]117.50.10.10'\n  - '[/baidupan.com/]117.50.10.10'\n  - '[/zhihu.com/]117.50.10.10'\n  - '[/zhihu.com/]117.50.10.10'\n  - '[/zhimg.com/]117.50.10.10'\n  - '[/cnki.net/]117.50.10.10'\n  - '[/iqiyi.com/]117.50.10.10'\n  - '[/qy.net/]117.50.10.10'\n  - '[/iqiyipic.com/]117.50.10.10'\n  - '[/acfun.cn/]117.50.10.10'\n  - '[/aixifan.com/]117.50.10.10'\n  - '[/yximgs.com/]117.50.10.10'\n  - '[/ksurl.cn/]117.50.10.10'\n  - '[/139.com/]117.50.10.10'\n  - '[/10086.cn/]117.50.10.10'\n  - '[/189.cn/]117.50.10.10'\n  - '[/knet.cn/]117.50.10.10'\n  - '[/10010.cn/]117.50.10.10'\n  - '[/10010.com/]117.50.10.10'\n  - '[/weibo.com/]117.50.10.10'\n  - '[/sina.com.cn/]117.50.10.10'\n  - '[/sohu.com/]117.50.10.10'\n  - '[/cctv.cn/]117.50.10.10'\n  - '[/nn.com/]117.50.10.10'\n  - '[/xunyou.com/]117.50.10.10'\n  - '[/leigod.com/]117.50.10.10'\n  - '[/steampowered.com/]117.50.10.10'\n  - '[/eccdnx.com/]117.50.10.10'\n  - '[/iplaysoft.com/]117.50.10.10'\n  - '[/jianshu.com/]117.50.10.10'\n  filtering_enabled: true\n  filters_update_interval: 24\n  parental_enabled: false\n  safesearch_enabled: false\n  safebrowsing_enabled: false\n  safebrowsing_cache_size: 1048576\n  safesearch_cache_size: 1048576\n  parental_cache_size: 1048576\n  cache_time: 30\n  rewrites: []\n  blocked_services: []\ntls:\n  enabled: false\n  server_name: \"\"\n  force_https: false\n  port_https: 443\n  port_dns_over_tls: 853\n  allow_unencrypted_doh: false\n  strict_sni_check: false\n  certificate_chain: \"\"\n  private_key: \"\"\n  certificate_path: \"\"\n  private_key_path: \"\"\nfilters: []\nwhitelist_filters: []\nuser_rules:\n- 192.108.239.108 video-weaver.lhr03.hls.ttvnw.net\n- \"\"\ndhcp:\n  enabled: false\n  interface_name: \"\"\n  gateway_ip: \"\"\n  subnet_mask: \"\"\n  range_start: \"\"\n  range_end: \"\"\n  lease_duration: 86400\n  icmp_timeout_msec: 1000\nclients: []\nlog_file: \"\"\nverbose: false\nschema_version: 6\n","  - tls://ea-dns.rubyfish.cn\n");
@@ -132,11 +177,11 @@ int Config_gen() {
 		else fprintf(yaml, "bind_host: 0.0.0.0\nbind_port: 80\nusers:\n- name: root\n  password: $2a$10$mdmUKhmmhuDQO8GHlU.8xOWzwG6Fy.5o5YUCUWYg2DkyfgYRsM0hy\nhttp_proxy: ""\nlanguage: ""\nrlimit_nofile: 0\nweb_session_ttl: 720\ndns:\n  bind_host: 0.0.0.0\n  port: 53\n  statistics_interval: 1\n  querylog_enabled: false\n  querylog_interval: 90\n  querylog_size_memory: 1000\n  anonymize_client_ip: false\n  protection_enabled: true\n  blocking_mode: default\n  blocking_ipv4: \"\"\n  blocking_ipv6: \"\"\n  blocked_response_ttl: 10\n  ratelimit: 0\n  ratelimit_whitelist: []\n  refuse_any: true\n  bootstrap_dns:\n  - 117.50.10.10\n  all_servers: true\n  edns_client_subnet: true\n  enable_dnssec: false\n  aaaa_disabled: false\n  allowed_clients: []\n  disallowed_clients: []\n  blocked_hosts: []\n  parental_block_host: family-block.dns.adguard.com\n  safebrowsing_block_host: standard-block.dns.adguard.com\n  cache_size: 0\n  cache_ttl_min: 0\n  cache_ttl_max: 0\n  upstream_dns:\n%s  - '[/netease.com/]117.50.10.10'\n  - '[/126.net/]117.50.10.10'\n  - '[/icourse163.org/]117.50.10.10'\n  - '[/yeah.net/]117.50.10.10'\n  - '[/126.com/]117.50.10.10'\n  - '[/163.com/]117.50.10.10'\n  - '[/zhangmuhan.club/]117.50.10.10'\n  - '[/google.cn/]117.50.10.10'\n  - '[/baidu.com/]117.50.10.10'\n  - '[/baidupcs.com/]117.50.10.10'\n  - '[/bdstatic.com/]117.50.10.10'\n  - '[/bilibili.com/]117.50.10.10'\n  - '[/bilivideo.com/]117.50.10.10'\n  - '[/hdslb.com/]117.50.10.10'\n  - '[/qq.com/]117.50.10.10'\n  - '[/douyu.com/]117.50.10.10'\n  - '[/huya.com/]117.50.10.10'\n  - '[/aliyun.com/]117.50.10.10'\n  - '[/alicdn.com/]117.50.10.10'\n  - '[/alipay.com/]117.50.10.10'\n  - '[/taobao.com/]117.50.10.10'\n  - '[/tmall.com/]117.50.10.10'\n  - '[/jd.com/]117.50.10.10'\n  - '[/zxxk.com/]117.50.10.10'\n  - '[/cnzz.com/]117.50.10.10'\n  - '[/chaoxing.com/]117.50.10.10'\n  - '[/snnu.edu.cn/]117.50.10.10'\n  - '[/ipip.net/]117.50.10.10'\n  - '[/7moor.com/]117.50.10.10'\n  - '[/t.cn/]117.50.10.10'\n  - '[/chinaz.com/]117.50.10.10'\n  - '[/uzer.me/]117.50.10.10'\n  - '[/lanzou.com/]117.50.10.10'\n  - '[/lanzous.com/]117.50.10.10'\n  - '[/baidupan.com/]117.50.10.10'\n  - '[/zhihu.com/]117.50.10.10'\n  - '[/zhihu.com/]117.50.10.10'\n  - '[/zhimg.com/]117.50.10.10'\n  - '[/cnki.net/]117.50.10.10'\n  - '[/iqiyi.com/]117.50.10.10'\n  - '[/qy.net/]117.50.10.10'\n  - '[/iqiyipic.com/]117.50.10.10'\n  - '[/acfun.cn/]117.50.10.10'\n  - '[/aixifan.com/]117.50.10.10'\n  - '[/yximgs.com/]117.50.10.10'\n  - '[/ksurl.cn/]117.50.10.10'\n  - '[/139.com/]117.50.10.10'\n  - '[/10086.cn/]117.50.10.10'\n  - '[/189.cn/]117.50.10.10'\n  - '[/knet.cn/]117.50.10.10'\n  - '[/10010.cn/]117.50.10.10'\n  - '[/10010.com/]117.50.10.10'\n  - '[/weibo.com/]117.50.10.10'\n  - '[/sina.com.cn/]117.50.10.10'\n  - '[/sohu.com/]117.50.10.10'\n  - '[/cctv.cn/]117.50.10.10'\n  - '[/nn.com/]117.50.10.10'\n  - '[/xunyou.com/]117.50.10.10'\n  - '[/leigod.com/]117.50.10.10'\n  - '[/steampowered.com/]117.50.10.10'\n  - '[/eccdnx.com/]117.50.10.10'\n  - '[/iplaysoft.com/]117.50.10.10'\n  - '[/jianshu.com/]117.50.10.10'\n  filtering_enabled: true\n  filters_update_interval: 24\n  parental_enabled: false\n  safesearch_enabled: false\n  safebrowsing_enabled: false\n  safebrowsing_cache_size: 1048576\n  safesearch_cache_size: 1048576\n  parental_cache_size: 1048576\n  cache_time: 30\n  rewrites: []\n  blocked_services: []\ntls:\n  enabled: false\n  server_name: \"\"\n  force_https: false\n  port_https: 443\n  port_dns_over_tls: 853\n  allow_unencrypted_doh: false\n  strict_sni_check: false\n  certificate_chain: \"\"\n  private_key: \"\"\n  certificate_path: \"\"\n  private_key_path: \"\"\nfilters: []\nwhitelist_filters: []\nuser_rules:\n- 192.108.239.108 video-weaver.lhr03.hls.ttvnw.net\n- \"\"\ndhcp:\n  enabled: false\n  interface_name: \"\"\n  gateway_ip: \"\"\n  subnet_mask: \"\"\n  range_start: \"\"\n  range_end: \"\"\n  lease_duration: 86400\n  icmp_timeout_msec: 1000\nclients: []\nlog_file: \"\"\nverbose: false\nschema_version: 6\n", "  - tls://1dot1dot1dot1.cloudflare-dns.com\n  - https://dns.cloudflare.com/dns-query\n");
 		fclose(yaml);
 		system("cls");
-		if(area==1)printf("已选定：%s\n\n","红鱼DNS(亚洲东部)");
-		else if (area == 2)printf("已选定：%s\n\n", "红鱼DNS(美国西部)");
-		else if (area == 3)printf("已选定：%s\n\n", "GoogleDNS");
-		else if (area == 4)printf("已选定：%s\n\n", "OpenDNS");
-		else if (area == 5)printf("已选定：%s\n\n", "AdGuardDNS");
-		else printf("已选定：%s\n\n", "CloudflareDNS");
+		if(area==1)printf("已选定：%s\n\n","红鱼DNS（亚洲东部）");
+		else if (area == 2)printf("已选定：%s\n\n", "红鱼DNS（美国西部）");
+		else if (area == 3)printf("已选定：%s\n\n", "GoogleDNS（中国香港）");
+		else if (area == 4)printf("已选定：%s\n\n", "OpenDNS（中国香港）");
+		else if (area == 5)printf("已选定：%s\n\n", "AdGuardDNS（日本东京）");
+		else printf("已选定：%s\n\n", "CloudflareDNS（日本大阪）");
 	return 0;
 }
